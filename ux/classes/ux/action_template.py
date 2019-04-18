a18d9bb5a23524b2a740207280af143c4faae12c
vahndi.minah <vahndi.minah@frogdesign.com>
2019-04-18 12:29:53 -0400

    update docs for ActionTemplate

--- ux/classes/ux/action_template.py
+++ ux/classes/ux/action_template.py
@@ -2,10 +2,19 @@ from ux.interfaces.ux.i_action_template import IActionTemplate
 
 
 class ActionTemplate(IActionTemplate):
-
+    """
+    Represents a Template for a single Action that could be taken by a User.
+    """
     def __init__(self, action_type: str, source_id: str, target_id: str = None,
                  weighting: float = 1):
+        """
+        Create a new ActionTemplate.
 
+        :param action_type: A type for the Action (used in matching)
+        :param source_id: The id of the Location where the Action would be taken (used in matching).
+        :param target_id: Optional id of the Location where the Action would go to (used in matching).
+        :param weighting: Optional weighting value to use in various calculations.
+        """
         self._action_type = action_type
         self._source_id = source_id
         self._target_id = target_id
@@ -13,18 +22,38 @@ class ActionTemplate(IActionTemplate):
 
     @property
     def action_type(self):
+        """
+        Return the type of the ActionTemplate.
+
+        :rtype: str
+        """
         return self._action_type
 
     @property
     def source_id(self):
+        """
+        Return the id of the Location where the Action would be taken.
+
+        :rtype: str
+        """
         return self._source_id
 
     @property
     def target_id(self):
+        """
+        Return the id of the Location where the Action would go to.
+
+        :rtype: str
+        """
         return self._target_id
 
     @property
     def weighting(self):
+        """
+        Return the weighting for the Action for various calculations.
+
+        :rtype: float
+        """
         return self._weighting
 
     def __eq__(self, other):
