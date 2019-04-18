26ef7afb45433bbcd81f29af1ef2135b38fb897e
vahndi.minah <vahndi.minah@frogdesign.com>
2019-04-18 15:09:33 -0400

    update docs for TaskResult

--- ux/classes/ux/task_result.py
+++ ux/classes/ux/task_result.py
@@ -4,38 +4,47 @@ from ux.calcs.basic_calcs.task_success import binary_task_success_rate
 
 
 class TaskResult(object):
+    """
+    Represents the Result of a Task.
+    """
+    def __init__(self, value, extra: dict = None):
+        """
+        Create a new Task Result.
 
-    def __init__(self, value, metadata: dict = None):
-
+        :param value: The numeric value of the result.
+        :param extra: Additional metadata about the TaskResult.
+        """
         self.value = value
-        self.metadata = metadata
+        self._extra = extra
 
     @staticmethod
     def binary_task_success_rate(results,
-                                 alpha: float = 0.05, method: str = 'normal',
-                                 name: str = None):
+                                 alpha: float = 0.05, method: str = 'normal'):
         """
+        Return the binary success rate of a number of TaskResults.
+
         :type results: List[TaskResult]
         :param results: list of pass (1/True) / fail (0/False) results
         :param alpha: significance level
         :param method: method to use for confidence interval
-        :param name: optional name
         :rtype: SuccessRate
+        :return: (mean, confidence_interval)
         """
         result_values = [result.value for result in results]
         return binary_task_success_rate(
             results=result_values, alpha=alpha,
-            method=method, name=name
+            method=method
         )
 
 
-r1 = (0.5, {'task': 'task 1', 'user': 'user 1'})
-r2 = (0.6, {'task': 'task 1', 'user': 'user 2'})
-r3 = (0.7, {'task': 'task 2', 'user': 'user 1'})
-r4 = (0.8, {'task': 'task 2', 'user': 'user 2'})
+if __name__ == '__main__':
 
+    r1 = (0.5, {'task': 'task 1', 'user': 'user 1'})
+    r2 = (0.6, {'task': 'task 1', 'user': 'user 2'})
+    r3 = (0.7, {'task': 'task 2', 'user': 'user 1'})
+    r4 = (0.8, {'task': 'task 2', 'user': 'user 2'})
 
-tr1 = TaskResult(*r1)
-tr2 = TaskResult(*r2)
-tr3 = TaskResult(*r3)
-tr4 = TaskResult(*r4)
+    tr1 = TaskResult(*r1)
+    tr2 = TaskResult(*r2)
+    tr3 = TaskResult(*r3)
+    tr4 = TaskResult(*r4)
