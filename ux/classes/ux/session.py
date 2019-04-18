83b9b9d0f65d3c24ed5f80506e4a3882110fb51e
vahndi.minah <vahndi.minah@frogdesign.com>
2019-04-18 12:35:21 -0400

    update docs for Session

--- ux/classes/ux/session.py
+++ ux/classes/ux/session.py
@@ -1,10 +1,22 @@
+from datetime import datetime
+
 from ux.interfaces.ux.i_session import ISession
 
 
 class Session(ISession):
+    """
+    Represents a single User Session.
+    """
+    def __init__(self, session_id: str, user_id: str,
+                 start_time: datetime, end_time: datetime):
+        """
+        Create a new Session.
 
-    def __init__(self, session_id, user_id, start_time, end_time):
-
+        :param session_id: The id of the Session.
+        :param user_id: The id of the User whose Session this is.
+        :param start_time: The start date-time of the Session.
+        :param end_time: The end date-time of the Session.
+        """
         self._session_id = session_id
         self._user_id = user_id or None
         self._start_time = start_time
@@ -12,18 +24,38 @@ class Session(ISession):
 
     @property
     def session_id(self):
+        """
+        Return the id of the Session.
+
+        :rtype: str
+        """
         return self._session_id
 
     @property
     def user_id(self):
+        """
+        Return the id of the Session's User.
+
+        :rtype: str
+        """
         return self._user_id
 
     @property
     def start_time(self):
+        """
+        Return the start date-time of the Session
+
+        :rtype: datetime
+        """
         return self._start_time
 
     @property
     def end_time(self):
+        """
+        Return the end date-time of the Session
+
+        :rtype: datetime
+        """
         return self._end_time
 
     def __repr__(self):
