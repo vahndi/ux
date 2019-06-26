7d415f2f570d695a6b69c1fd1b4a0e2a4ec4b134
vahndi.minah <vahndi.minah@frogdesign.com>
2019-06-26 13:14:08 -0400

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ux',
-    version='0.1.2',
+    version='0.1.3',
     packages=['ux', 'ux.calcs', 'ux.plots', 'ux.classes', 'ux.classes.ux', 'ux.classes.stats', 'ux.interfaces',
               'ux.interfaces.ux', 'tests', 'examples'],
     url='https://github.com/vahndi/ux',
