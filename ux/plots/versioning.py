9a2756a99b918e9b01133203eee350839acb17b7
VXM6870 <vahndi.minah@frogdesign.com>
2019-07-01 10:16:08 -0400

    add optional date range to location history

--- ux/plots/versioning.py
+++ ux/plots/versioning.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from matplotlib.axes import Axes
 
 from ux.interfaces.i_database_manager import IDatabaseManager
@@ -5,18 +6,31 @@ from ux.plots.helpers import new_axes
 from ux.utils.versioning import find_location_history
 
 
-def plot_location_history(manager: IDatabaseManager, ax: Axes = None):
+def plot_location_history(manager: IDatabaseManager, start: datetime = None, end: datetime = None, ax: Axes = None):
     """
     Plot the history of each Location's appearance in the set of logs in Manager.
 
     :param manager: Child of IDatabaseManager containing the logs to use.
+    :param start: Optional start date-time to exclude older sessions.
+    :param end: Optional end date-time to exclude newer sessions.
     :param ax: Optional matplotlib axes to plot on.
     :rtype: Axes
     """
     ax = ax or new_axes()
-    history = find_location_history(manager)
-    for l, location in enumerate(history.keys()):
+    history = find_location_history(manager=manager, start=start, end=end)
+    locations = list(history.keys())
+    for l, location in enumerate(locations):
         ax.scatter(
             x=history[location],
             y=[l] * len(history[location]),
+            marker='.'
         )
+    ax.set_yticks(range(1, len(locations)))
+    ax.set_yticklabels(locations)
+    if start:
+        x_lim = ax.get_xlim()
+        ax.set_xlim(start, x_lim[1])
+    if end:
+        x_lim = ax.get_xlim()
+        ax.set_xlim(x_lim[0], end)
+
--- ux/utils/versioning.py
+++ ux/utils/versioning.py
@@ -1,20 +1,25 @@
 from collections import defaultdict
+from datetime import datetime
 
 from ux.interfaces.actions.i_action_sequence import IActionSequence
 from ux.interfaces.i_database_manager import IDatabaseManager
 
 
-def find_location_history(manager: IDatabaseManager):
+def find_location_history(manager: IDatabaseManager, start: datetime = None, end: datetime = None):
     """
     Find the history of each Location's appearance in the Database.
 
     :param manager: Instance of a class inheriting from IDatabaseManager.
+    :param start: Optional start date-time to exclude older sessions.
+    :param end: Optional end date-time to exclude newer sessions.
     :return: Dictionary mapping location ids to lists of session start times.
     :rtype: dict
     """
     history = defaultdict(list)
     for session in manager.sessions():
         session_start = session.start_time
+        if (start and session_start < start) or (end and (session_start > end)):
+            continue
         sequence: IActionSequence = manager.get_session_sequence(session_id=session.session_id)
         for location_id in sequence.location_ids():
             history[location_id].append(session_start)
